827bca6d3c80209c5e972902ed8d30248326f277
Better_OIer <2155173747@qq.com>
2022-12-08 17:00:26 +0800

    20221208170

---
+++ Project/test/main.cpp
@@ -0,0 +1,18 @@
+#include<easyx.h>
+#include<cstdlib>
+const int X=1366;
+const int Y=768;
+int main(){
+    
+	initgraph(X,Y);
+    setorigin(X>>1,Y>>1);
+    setaspectratio(1, -1);
+	for(int i = (Y>>1);i>=0;i--){
+        setfillcolor(RGB(255, 255, i));
+        setlinecolor(RGB(255, 255, i));
+        fillcircle(0,0,i);
+        Sleep(10);
+    }
+    system("pause");
+	return 0;
+}
