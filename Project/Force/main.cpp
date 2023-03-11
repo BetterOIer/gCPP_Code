dc55a666d12cb42adb2ffc33c6ffab7ee077725f
Better_OIer <2155173747@qq.com>
2023-03-11 15:02:21 +0800

    202303111502

--- Project/Force/main.cpp
+++ Project/Force/main.cpp
@@ -18,28 +18,62 @@ inline int read(){
 	for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);
 	return x*f;
 }
-int po_x=0,po_y=0;
-bool likai=false;
-struct xl{
-    int len=0;
-    int angle=0;
+struct node{
+    int x;
+    int y;
 };
-vector <xl> P;
-void analyze(){
-    
+vector <node> P;
+double vx=-10,vy=0;
+double x=1366,y=180;
+void merge(double &F,double &X,double &Y,double fn,double xn,double yn){
+	F=F*F+fn*fn+fn*F*2*((X/sqrt(X*X+Y*Y))*(xn/sqrt(xn*xn+yn*yn))+(Y/sqrt(X*X+Y*Y))*(yn/sqrt(xn*xn+yn*yn)));
+	X=X+xn;
+	Y=Y+yn;
 }
-void merge(){
-    
-}
-void print(){
-    
+void pri(){
+	double F=0,X=-1,Y=0;
+    for(node f:P){
+		circle(f.x, f.y, 5);
+        double f1=500/((f.x-x)*(f.x-x)+(f.y-y)*(f.y-y));
+		merge(F,X,Y,f1,f.x-x,f.y-y);
+    }
+	vx+=F*X/sqrt(X*X+Y*Y);
+	vy+=F*Y/sqrt(X*X+Y*Y);
 }
 int main(){
     initgraph(1366,768);
-    while(true){
-        analyze();
-        merge();
-        print();
-    }
-    system("pause");
+	line(1366,200,0,200);
+    ExMessage m;		// 定义消息变量
+
+	while(true)
+	{
+		// 获取一条鼠标或按键消息
+		m = getmessage(EX_MOUSE | EX_KEY);
+		switch(m.message){
+			case WM_LBUTTONDOWN:
+				// 画一个小方块
+				setfillcolor(BLACK);
+				fillrectangle(0,0,1366,768);
+				setfillcolor(WHITE);
+                circle(m.x, m.y, 5);
+				x=1366,y=200;
+				vx=-10,vy=0;
+                P.push_back((node){m.x,m.y});
+                pri();
+				while(x>=0&&x<=1366&&y>=0&&y<=768){
+					x+=vx;
+					y+=vy;
+					fillcircle(x, y, 4);
+					Sleep(4);
+					pri();
+				}
+				break;
+
+			case WM_KEYDOWN:
+				if (m.vkcode == VK_ESCAPE)
+					return 0;	// 按 ESC 键退出程序
+		}
+	}
+	closegraph();
+	return 0;
 }
